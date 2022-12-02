9e389278b8210126e1d1cc2278899c4b63f5042b
aisling-2 <112478306+aisling-2@users.noreply.github.com>
2022-12-02 11:14:17 +0900

    rsa signature shell examples

---
+++ shell/rsa_signature_post_order.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Set up authentication with your API key:
+apiKey=""
+
+# Replace with your RSA PKCS#8 private key path:
+privKeyPath = "/Users/alice/ssh/key.pem"
+
+# Set up the request:
+baseUrl="https://testnet.binance.vision"
+apiMethod="POST"
+apiCall="api/v3/order"
+apiParams="symbol=BTCUSDT&side=SELL&type=LIMIT&timeInForce=GTC&quantity=1&price=0.2&recvWindow=5000"
+
+
+function rawurlencode {
+    local value="$1"
+    local len=${#value}
+    local encoded=""
+    local pos c o
+    for (( pos=0 ; pos<len ; pos++ ))
+    do
+        c=${value:$pos:1}
+        case "$c" in
+            [-_.~a-zA-Z0-9] ) o="${c}" ;;
+            * )   printf -v o '%%%02x' "'$c"
+        esac
+        encoded+="$o"
+    done
+    echo "$encoded"
+}
+
+ts=$(date +%s000)
+paramsWithTs="$apiParams&timestamp=$ts"
+
+rawSignature=$(echo -n "$paramsWithTs" \
+               | openssl dgst -keyform PEM -sha256 -sign $privKeyPath \
+               | openssl enc -base64 \
+               | tr -d '\n')
+signature=$(rawurlencode "$rawSignature")
+
+curl -H "X-MBX-APIKEY: $apiKey" -X $apiMethod \
+    "$baseUrl/$apiCall?$paramsWithTs&signature=$signature"
+
